99c4058ab3e0179d271f5d147cd936a4bb660521
Evgeniy.A <majik48ru@gmail.com>
2021-07-18 18:48:54 +0300

    step 7

--- src/games/brain-gcd-game.js
+++ src/games/brain-gcd-game.js
@@ -5,16 +5,23 @@ const minNum = 1;
 const maxNum = 100;
 const numberOfRounds = 3;
 const getRandomNum = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-
-const evenGame = () => {
+const greatestComDiv = (num1, num2) => {
+  if (num2 > 0) {
+    const k = num1 % num2;
+    return greatestComDiv(num2, k);
+  }
+  return Math.abs(num1);
+};
+const gcdGame = () => {
   const userName = getTheName();
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  console.log('Find the greatest common divisor of given numbers.');
   for (let i = 1; i <= numberOfRounds; i += 1) {
-    const question = getRandomNum(minNum, maxNum);
-    const isEven = question % 2 === 0;
+    const num1 = getRandomNum(minNum, maxNum);
+    const num2 = getRandomNum(minNum, maxNum);
+    const question = `${num1} ${num2}`;
     console.log(`Question: ${question}`);
-    const answer = String(readlineSync.question('Your answer: '));
-    const correctAnswer = isEven ? 'yes' : 'no';
+    const answer = Number(readlineSync.question('Your answer: '));
+    const correctAnswer = greatestComDiv(num1, num2);
     if (answer === correctAnswer) {
       console.log('Correct!');
     } else {
@@ -23,4 +30,4 @@ const evenGame = () => {
   }
   return console.log(`Congratulations, ${userName}!`);
 };
-export default evenGame;
+export default gcdGame;
